a6c1048cac5932e8583121fd8b7866c7bdb5858c
Mila <59932809+milamur@users.noreply.github.com>
2020-04-13 12:00:26 -0400

    Create function-arguments.js

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math(firstArgument, secondArgument, thirdArgument) {
+  return secondArgument * thirdArgument + firstArgument
+}
+console.log(math(firstArgument = 53, secondArgument = 61, thirdArgument = 67))
